b4333b46afc30cd0a3a625e8fa117212052305c4
happy663 <53854298+happy663@users.noreply.github.com>
2021-05-06 04:53:57 +0900

    フロント環境構築

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,15 +1,31 @@
-from flask import Flask
-from redis import Redis
+from flask import Flask, render_template, request, jsonify
+# from redis import Redis
 
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+# redis = Redis(host='redis', port=6379)
+
+# @app.route('/')
+# def hello():
+#     redis.incr('hits')
+#     return "Hello"
 
 
 @app.route('/')
-def hello():
-    redis.incr('hits')
+def Hello():
     return "Hello"
+
+
+
+@app.route('/route')
+def route():
+    return "route"
+
+
+@app.route("/hoge", methods=['GET'])
+def getHoge():
+    return jsonify({"lang": "Python"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
