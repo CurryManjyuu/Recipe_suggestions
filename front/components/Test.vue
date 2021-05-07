8dc7dace4a001c3559ae0eb2090da467b3f492da
Furugen, Motoshi <amanogawa_m@icloud.com>
2021-05-07 21:30:53 +0900

    selectフォルダ作成

--- front/components/Test.vue
+++ front/components/Test.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>ルーティング設定しなくていい</h1>
-  </div>
+ <div class="page">
+   <p>{{ posts }}</p>
+ </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+ data () {
+   return {
+     posts: [],
+   }
+ },
+ mounted () {
+   axios.get('https://jsonplaceholder.typicode.com/posts')
+     .then((response) => this.posts = response)
+ }
+}
+</script>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <h1>Hello world!</h1>
-    <NuxtLink to="/fun">Home page</NuxtLink>
-    <Logo></Logo>
-    <VuetifyLogo></VuetifyLogo>
-    <God></God>
-    <Home></Home>
-    <Test></Test>
+    <h1>今日のレシピ</h1>
+    <!-- <NuxtLink to="/fun">Home page</NuxtLink> -->
+    <!-- <Logo></Logo> -->
+    <!-- <VuetifyLogo></VuetifyLogo> -->
+    <!-- <God></God> -->
+    <!-- <Home></Home> -->
+    <v-btn to="/select/start" x-large color="pink accent-4" block class="white--text"><b>何つくろう</b></v-btn>
+    <!-- <Test></Test> -->
   </div>
 </template>
 
