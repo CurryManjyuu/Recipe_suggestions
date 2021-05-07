a73fd8e78244562fe0209aa6828616f7d263e068
happy663 <53854298+happy663@users.noreply.github.com>
2021-05-07 19:05:07 +0900

    CORSエラーの解消

--- front/components/Home.vue
+++ front/components/Home.vue
@@ -5,6 +5,8 @@
     <button @click="hantei = !hantei">sleep</button>
     <p v-if="hantei">{{ toyamabaka }}</p>
     <p v-else>{{ toyama }}</p>
+    <button @click="apiTest">apiTest</button>
+    <p>{{ posts }}</p>
   </div>
 </template>
 
@@ -15,13 +17,20 @@ export default {
       hello: "hello2",
       toyama: "toyamaは天才",
       toyamabaka: "toyamaはバカ",
-      hantei: true
+      hantei: true,
+      posts: []
     };
   },
   methods: {
     apiTest: function() {
-      
-    }
+      this.$axios.$get("http://localhost:5000/hoge").then(response => {
+        console.log("response", response);
+        this.posts = response;
+      });
+    },
+    mounted() {}
+    //https://date.nager.at/api/v2/publicholidays/2017/AT
+    //https://pokeapi.co/api/v2/ability/7/")
   }
 };
 </script>
