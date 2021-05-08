c5fd5282dc8cca0107f50dca02ae1365a36d5d8d
happy663 <53854298+happy663@users.noreply.github.com>
2021-05-08 14:32:37 +0900

    途中まで

--- front/components/Login.vue
+++ front/components/Login.vue
@@ -3,8 +3,8 @@
     <div class="text-center">
       <v-dialog v-model="dialog" width="500">
         <template v-slot:activator="{ on, attrs }">
-          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
-            Click Me
+          <v-btn color="blue lighten-2" dark v-bind="attrs" v-on="on">
+            ログイン
           </v-btn>
         </template>
         <v-card>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1>今日のレシピ</h1>
-    <!-- <NuxtLink to="/fun">Home page</NuxtLink> -->
-    <!-- <God></God> -->
-    <!-- <Home></Home> -->
-    <v-btn
-      to="/select/start"
-      x-large
-      color="pink accent-4"
-      block
-      class="white--text"
-      ><b>何つくろう</b></v-btn
-    >
+    <v-app-bar>
+      <h1>今日のレシピ</h1>
+      <v-btn
+        to="/select/start"
+        x-large
+        color="pink accent-4"
+        block
+        class="white--text"
+        ><b>何つくろう</b></v-btn
+      >
+      <Login style="position:absolute;top:5px;right:70px"></Login>
+    </v-app-bar>
   </div>
 </template>
 
