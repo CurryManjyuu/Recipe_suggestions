d08ccfb898128fb84ab679949270a4215a74f848
Furugen, Motoshi <amanogawa_m@icloud.com>
2021-05-08 22:38:59 +0900

    テキスト微修正

--- front/pages/Select/Start.vue
+++ front/pages/Select/Start.vue
@@ -6,7 +6,7 @@
           <v-btn rounded to="/select/categories" x-large color="orange accent-4" block class="white--text"><b>材料から選ぶ</b></v-btn>
         </v-col>
         <v-col cols="12" sm="12" md="4" lg="4">
-          <v-btn rounded to="/select/method" x-large color="green accent-4" block class="white--text"><b>作り方から選ぶ</b></v-btn>
+          <v-btn rounded to="/select/method" x-large color="green accent-4" block class="white--text"><b>シーンから選ぶ</b></v-btn>
         </v-col>
         <v-col cols="12" sm="12" md="4" lg="4">
           <v-btn rounded to="/select/feeling" x-large color="light-blue accent-4" block class="white--text"><b>気分から選ぶ</b></v-btn>
