57c38934cab6e38acc98b17091f92b6fa14c4d8b
Furugen, Motoshi <amanogawa_m@icloud.com>
2021-05-09 15:29:05 +0900

    ボタン色変更

--- front/pages/Select/_recipe.vue
+++ front/pages/Select/_recipe.vue
@@ -3,13 +3,13 @@
     <v-container>
       <v-row class="center" style="margin-bottom:1.5em;">
         <v-col cols="12" sm="12" md="4" lg="4">
-          <v-btn rounded to="/select/categories" x-large color="orange accent-4" block class="white--text"><b>材料から選ぶ</b></v-btn>
+          <v-btn rounded to="/select/categories" x-large v-bind:color="color.category" block v-bind:class="categoryText"><b>材料から選ぶ</b></v-btn>
         </v-col>
         <v-col cols="12" sm="12" md="4" lg="4">
-          <v-btn rounded to="/select/method" x-large color="green accent-4" block class="white--text"><b>シーンから選ぶ</b></v-btn>
+          <v-btn rounded to="/select/method" x-large v-bind:color="color.method" block v-bind:class="methodText"><b>シーンから選ぶ</b></v-btn>
         </v-col>
         <v-col cols="12" sm="12" md="4" lg="4">
-          <v-btn rounded to="/select/feeling" x-large color="light-blue accent-4" block class="white--text"><b>地域から選ぶ</b></v-btn>
+          <v-btn rounded to="/select/feeling" x-large v-bind:color="color.feeling" block v-bind:class="feelingText"><b>地域から選ぶ</b></v-btn>
         </v-col>
       </v-row>
       <v-row class="center">
@@ -69,10 +69,37 @@ export default {
     return {
       posts: [],
       categoryId: "",
+      color: {
+        category : "orange accent-4",
+        method : "green accent-4",
+        feeling : "light-blue accent-4"
+      },
+      categoryId : {
+        category : ["30", "31", "32", "33", "14", "15", "16", "17", "23", "18", "22", "21", "10", "11", "12", "34", "19", "27", "35", "13"],
+        method : ["20", "36", "37", "38", "39", "40", "24", "49", "50", "51", "52", "53", "54", "55", "26"],
+        feeling : ["41", "42", "43", "44", "25", "46", "47", "48"]
+      },
+      categoryText: "white--text",
+      methodText: "white--text",
+      feelingText: "white--text"
     };
   },
-  // アクセス時に一度APIを叩いて4件取得する。
   mounted () {
+    // パラメータからカテゴリ群を取得してボタンの色を変更する。
+    function inArray(value, array) {
+      return [].indexOf.call(array, value);
+    }
+    if (inArray(this.$route.params.recipe, this.categoryId.category) >= 0) {
+      this.color.category = "orange accent-1";
+      this.categoryText = "deep-orange--text";
+    } else if (inArray(this.$route.params.recipe, this.categoryId.method) >= 0) {
+      this.color.method = "green accent-2";
+      this.methodText = "teal--text"
+    } else if (inArray(this.$route.params.recipe, this.categoryId.feeling) >= 0) {
+      this.color.feeling = "light-blue accent-1";
+      this.feelingText = "indigo--text";
+    };
+    // アクセス時に一度APIを叩いて4件取得する。
     axios
       .get("http://localhost:5000/recipe/large/"+this.$route.params.recipe+"/"+0)
       .then(response => (this.posts = response.data))
